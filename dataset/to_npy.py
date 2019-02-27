2d1f9b2f0d6066b444e00e064d70fc42a6b41d48
tadax <tadax@xtn.jp>
2019-02-28 02:57:53 +0900

    Add resizing

--- dataset/to_npy.py
+++ dataset/to_npy.py
@@ -11,9 +11,15 @@ def main(args):
         dst = os.path.join(args.output_dir, filename)
         if os.path.exists(dst):
             continue
-        npimg = np.fromfile(path, dtype=np.uint8)
-        img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        try:
+            npimg = np.fromfile(path, dtype=np.uint8)
+            img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (args.imgsize, args.imgsize),
+                             interpolation=cv2.INTER_CUBIC)
+        except:
+            print('Invalid: {}'.format(path))
+            continue
         np.save(dst, img)
         print('{}/{} - {}'.format(i+1, len(paths), dst))
 
@@ -21,4 +27,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', required=True)
     parser.add_argument('--output_dir', required=True)
+    parser.add_argument('--imgsize', type=int, default=512)
     main(parser.parse_args())
